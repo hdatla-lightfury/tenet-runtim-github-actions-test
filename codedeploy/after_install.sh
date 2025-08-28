62eaf06ab4533b396a07010d49d1e6b8dfcf271b
hdatla-lightfury <harish@lightfurygames.com>
2025-08-28 15:30:55 +0530

    fixing more stuff

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -6,6 +6,9 @@ set -e
 
 sudo chmod -R 666 /home/ec2-user/tenet-runtime/*
 
+sudo chown ec2-user:ec2-user backend.so
+
+
 SECRET_ID="staging/tenet-runtime/aws-rds-postgres"
 AWS_REGION="ap-south-1"
 
